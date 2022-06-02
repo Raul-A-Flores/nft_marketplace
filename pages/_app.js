b777ff616fc5b5b94194237915ff191cae182bc0
raul <raul.alexander.floress@gmail.com>
2022-06-02 16:58:19 -0500

    create navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
 
-export default MyApp
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/7a7a619281.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
---
+++ pages/create-nft.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateNFT = () => (
+  <div>create_nft</div>
+);
+
+export default CreateNFT;
