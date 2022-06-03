8d7838fa07ad485bed3840e5df9ef1c67bb3be34
raul <raul.alexander.floress@gmail.com>
2022-06-03 10:25:45 -0500

    add main page create nft page

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
 import NFTCard from './NFTCard';
+import Input from './Input';
 
-export { Navbar, Footer, Button, Banner, NFTCard };
+export { Navbar, Footer, Button, Banner, NFTCard, Input };
